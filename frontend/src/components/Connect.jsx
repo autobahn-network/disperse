b277d8483aaff898f0bc50fddf5763199930d2a6
Christian Eichinger <christianeichinger@MacBook-Pro-von-Christian.local>
2022-11-25 15:12:22 +0100

    UI finetuning

--- frontend/src/components/Connect.jsx
+++ frontend/src/components/Connect.jsx
@@ -1,7 +1,7 @@
 const Connect = ({ connect }) => {
   return (
     <div className="pt-16">
-      <h3 className="text-2xl font-light italic">connect to wallet</h3>
+      <h3 className="text-2xl font-light italic">1. Wallet Connection</h3>
       <button
         onClick={connect}
         className="px-2 mt-6 p-2 italic"
